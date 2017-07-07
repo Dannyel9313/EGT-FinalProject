b742e1422e8e8a529cb85aa58208fc4c71af628a
Mario Mishev <mario@debian.debian>
2017-07-07 14:38:35 +0300

    Add XML recovery

--- KenoProject/src/XML.cpp
+++ KenoProject/src/XML.cpp
@@ -1,20 +1,38 @@
 #include "XML.h"
 
-XML::XML() {
+XML::XML() {}
+
+const char* XML::ToString(int in_val) const;
+{
+	std::string s = boost::lexical_cast<std::string> (in_val);
+	return s.c_str();
 }
 
-XML::XML(const char * file) {
-	doc.load_file(file);
+void XML::write(int credits, int array[8][10]) 
+{
+	//Specification node
+	pugi::xml_node rootNode = doc.append_child(pugi::node_declaration);
+    	rootNode.append_attribute("version")    = "1.0";
+    	rootNode.append_attribute("encoding")   = "ISO-8859-1";
+
+	//Main node
+	pugi::xml_node node = doc.append_child("Recovery");
+
+	//Credits
+	pugi::xml_node descr = node.append_child("Credits");
+	descr.append_child(pugi::node_pcdata).set_value(ToString(credits));
+
+	//Flags
+	pugi::xml_node flags = node.append_child("UserChoices");
+	for (int i = 0; i < 8; i++) 
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			flags.append_child(pugi::node_pcdata).set_value(ToString(array[i][j]));
+		}
+	}
+
+	doc.save_file("Recovery.xml");
 }
 
-void XML::write(const char * path) {
-	pugi::xml_node declarationNode = doc.append_child(pugi::node_declaration);
-    	declarationNode.append_attribute("version")    = "1.0";
-    	declarationNode.append_attribute("encoding")   = "ISO-8859-1";
-	pugi::xml_node node = doc.append_child("node");
-	pugi::xml_node descr = node.append_child("description");
-	descr.append_child(pugi::node_pcdata).set_value("simple node");
-	pugi::xml_node param = node.insert_child_before("param", descr);
-	param.append_attribute("name") = "version";
-	doc.save_file(path);
-}
+
--- KenoProject/src/XML.h
+++ KenoProject/src/XML.h
@@ -2,16 +2,27 @@
 #define XML_H_
 #include <iostream>
 #include "pugixml.hpp"
-#include "pugiconfig.hpp"
 #include <fstream>
+#include <boost/lexical_cast.hpp>
 
 class XML {
 	public:
+		//Constructor
 		XML();
-		XML(const char *);
-		void traverse() const;
-		void write(const char *);
+
+		//Read saved credits
+		void readCredit() const;
+	
+		//Read saved user choices
+		void readUserChoices() const;
+	
+		//Write in XML receive Credits(int), FlagsForPosition(int [][]);
+		void write(int, int [8][10]);
 	private:
+		//Utility function to convert int to string
+		const char* ToString(int) const;
+
+		//Documents
 		pugi::xml_document doc;	
 };
 
