37e95f6f0ffa140e0ce00656d9a712ba9c45321c
Mario Mishev <mario@debian.debian>
2017-07-11 09:30:21 +0300

    Convention stuff

--- KenoProject/src/NumbersGrid.cpp
+++ KenoProject/src/NumbersGrid.cpp
@@ -2,6 +2,7 @@
 
 NumbersGrid::NumbersGrid()
 {
+	//TODO NULL FONT, SOUND EFFECT
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -15,7 +16,7 @@ NumbersGrid::NumbersGrid()
 void NumbersGrid::createRects(SDL_Renderer * renderer)
 {
 	int xPos = getKRect().x, yPos = getKRect().y;
-	SDL_Rect tempRect = {xPos, yPos, 30, 40};
+	SDL_Rect tempRect = {xPos, yPos, 40, 30};
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -23,8 +24,12 @@ void NumbersGrid::createRects(SDL_Renderer * renderer)
 			//Set new rect
 			m_numbers[i][j] = tempRect;
 
-			roundedBoxRGBA(renderer, m_numbers[i][j].x, m_numbers[i][j].y, m_numbers[i][j].x+40, m_numbers[i][j].y+30, 5, 
-				225, 16, 16, 255);
+			roundedBoxRGBA(renderer, 
+				m_numbers[i][j].x, 
+				m_numbers[i][j].y, 
+				m_numbers[i][j].x+40, 
+				m_numbers[i][j].y+30, 
+				5, 225, 16, 16, 255);
 
 			//Set new position
 			xPos += 42;
@@ -60,30 +65,59 @@ void NumbersGrid::numbersInRects(SDL_Renderer * renderer)
 	{
 		for (int j = 0; j < 10; j++)
 		{
+			//1-digit numbers require specific spacing
 			if(number < 10) 
 			{
+				//Set the destination rect
 				dst_rect.x = m_numbers[i][j].x+13;
 				dst_rect.y = m_numbers[i][j].y+5;	
 				dst_rect.w = 24;	
 				dst_rect.h = 22;
+
+				//Sets outline to no outline
 				TTF_SetFontOutline(m_font, 0);
-				loadTextureFromTTF(toStringPlusSpace(number), renderer, m_font, white);
+
+				//Load text from font
+				loadTextureFromTTF(toStringPlusSpace(number), 
+					renderer, m_font, white);
+				
+				//Render
 				render(renderer, &dst_rect);  
+				
+				//Sets outline to 1
 				TTF_SetFontOutline(m_font, 1);
-				loadTextureFromTTF(toStringPlusSpace(number), renderer, m_font, black);
+
+				//Load text from font for outline
+				loadTextureFromTTF(toStringPlusSpace(number), 
+					renderer, m_font, black);
+				
+				//Render
 				render(renderer, &dst_rect); 
 			}
 			else 
-			{	
+			{
+				//Set destination rect	
 				dst_rect.x = m_numbers[i][j].x+9;
 				dst_rect.y = m_numbers[i][j].y+5;	
 				dst_rect.w = 24;	
 				dst_rect.h = 22;
+
+				//Set outline to no outline
 				TTF_SetFontOutline(m_font, 0);
+
+				//Load number from font
 				loadTextureFromTTF(toString(number), renderer, m_font, white);
+
+				//Render
 				render(renderer, &dst_rect);  
+
+				//Set outline to 1
 				TTF_SetFontOutline(m_font, 1);
+
+				//Load number from font
 				loadTextureFromTTF(toString(number), renderer, m_font, black);
+
+				//Render
 				render(renderer, &dst_rect); 
 			}
 			number++;			
@@ -97,26 +131,40 @@ void NumbersGrid::doIfClicked(SDL_Renderer * renderer, const SDL_Event & e)
 	{
 		for (int j = 0; j < 10; j++)
 		{
-			if(isClickedFlags[i][j] == 0 && isClicked(e, &m_numbers[i][j])) 
+			if(isClickedFlags[i][j] == 0 && 
+				isClicked(e, &m_numbers[i][j])) 
 			{
 				//Draw and fill rect
 				isClickedFlags[i][j] = 1;
-				roundedBoxRGBA(renderer, m_numbers[i][j].x, m_numbers[i][j].y, m_numbers[i][j].x+40, m_numbers[i][j].y+30, 5, 
-					255, 255, 0, 255);
+				roundedBoxRGBA(renderer, 
+					m_numbers[i][j].x, 
+					m_numbers[i][j].y, 
+					m_numbers[i][j].x+40,
+					m_numbers[i][j].y+30, 
+					5, 255, 255, 0, 255);
+
+				//Reapply numbers in rects
 				numbersInRects(renderer);
 
 				//Play sound effect
 				Mix_PlayChannel(-1, m_ClickEffect, 0);
 			}
-			else if (isClickedFlags[i][j] == 1 && isClicked(e, &m_numbers[i][j]))
+			else if (isClickedFlags[i][j] == 1 && 
+					isClicked(e, &m_numbers[i][j]))
 			{
 				//Draw and fill rect
 				isClickedFlags[i][j] = 0;
-				roundedBoxRGBA(renderer, m_numbers[i][j].x, m_numbers[i][j].y, m_numbers[i][j].x+40, m_numbers[i][j].y+30, 5, 
-					225, 16, 16, 255);
+				roundedBoxRGBA(renderer,
+					m_numbers[i][j].x, 
+					m_numbers[i][j].y,
+					m_numbers[i][j].x+40,
+					m_numbers[i][j].y+30, 
+					5, 225, 16, 16, 255);
+
+				//Reapply numbers in rects
 				numbersInRects(renderer);
 
-				//Play sound effect
+				//Play sound effect	
 				Mix_PlayChannel(-1, m_ClickEffect, 0);
 			}	
 		}
@@ -137,26 +185,39 @@ const char * NumbersGrid::toStringPlusSpace(int in_val)
 	return str.c_str();
 }
 
-void NumbersGrid::pickRandomNumbers(SDL_Renderer * renderer, const SDL_Event & e)
+void NumbersGrid::pickRandomNumbers(SDL_Renderer * renderer, 
+					const SDL_Event & e)
 {
 	SDL_Rect rect = {0, 0, 30, 30};
-	int r1;
-	int r2;
+	int rand_1;
+	int rand_2;
 	if (isClicked(e, &rect))
 	{
 		resetRandFlags();
 		for (int i = 0; i < 10; i++)
 		{
-			r1 = rand()%8;
-			r2 = rand()%10;
+			rand_1 = rand()%8;
+			rand_2 = rand()%10;
 			if (randomNumbersFlags[r1][r2] == 0) 
 			{
 				randomNumbersFlags[r1][r2] = 1;
-				thickLineRGBA (renderer, m_numbers[r1][r2].x, m_numbers[r1][r2].y+28,
-					 m_numbers[r1][r2].x+38, m_numbers[r1][r2].y, 3, 0, 0, 0, 50);
-				thickLineRGBA (renderer, m_numbers[r1][r2].x, m_numbers[r1][r2].y,
-					 m_numbers[r1][r2].x+38, m_numbers[r1][r2].y+28, 3, 0, 0, 0, 50);
+
+				//Draw diagonal lines
+				thickLineRGBA (renderer, 
+					m_numbers[rand_1][rand_2].x,
+					m_numbers[rand_1][rand_2].y+28,
+					m_numbers[rand_1][rand_2].x+38,
+					m_numbers[rand_1][rand_2].y, 
+					3, 0, 0, 0, 50);
+
+				thickLineRGBA (renderer, 
+					m_numbers[rand_1][rand_2].x,
+					m_numbers[rand_1][rand_2].y,
+					m_numbers[rand_1][rand_2].x+38, 
+					m_numbers[rand_1][rand_2].y+28,
+					3, 0, 0, 0, 50);
 			}
+			//If already flag up increase loop by 1 time
 			else	
 			{
 				i--;
@@ -176,18 +237,18 @@ void NumbersGrid::resetRandFlags()
 	}
 }
 
-bool NumbersGrid::loadSoundEffect(str::string path)
+bool NumbersGrid::loadSoundEffect(std::string path)
 {
-	bool success = true;
+	bool success = true;	
 
 	//Load effect from wav
 	m_ClickEffect = Mix_LoadWAV(path.c_str());
-	if(m_ClickEffect == NULL)
+	if (m_ClickEffect == NULL)
 	{
-		std::cout << "Error loading clicking effect" << std:endl;
+		std::cout << "Error loading clicking effect" << std::endl;
 		success = false;
 	}
-	
+
 	return success;
 }
 
@@ -205,5 +266,5 @@ int NumbersGrid::numberOfHits()
 			}
 		}
 	}
-	return count;
+	return count;		
 }
