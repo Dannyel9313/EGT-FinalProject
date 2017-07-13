1ac948f020b2cdafcc2fbc6c11556fc68e5d2d6d
Tishkata <tih4o_970@abv.bg>
2017-07-13 16:19:14 +0300

    Add files via upload

--- KenoProject/src/Win.cpp
+++ KenoProject/src/Win.cpp
@@ -1,3 +1,10 @@
+/*
+ * Win.cpp
+ *
+ *  Created on: Jul 11, 2017
+ *      Author: Memmory Of Tomorrow
+ */
+
 #include "Win.h"
 
 Win::Win()
@@ -12,33 +19,33 @@ Win::~Win()
 
 void Win::writeOnScreen(SDL_Renderer* renderer)
 {
-	loadTextureFromFile("Resources/Images/blueScreen.gif", renderer);
+	Mix_Chunk* winningMusic = NULL;
+	winningMusic = Mix_LoadWAV("Victory sound effect.wav");
+	if(winningMusic == NULL){
+		std::cerr << "Music file could not be loaded" << std::endl;
+	}
+	Mix_PlayChannel(-1, winningMusic, 0);
+	loadTexture("blueScreen.gif", renderer);
 	render(renderer, NULL);
 
-	firstText.setButtonColor(255, 0, 0);
-	secondText.setButtonColor(255, 0, 0);
-	thirdText.setButtonColor(255, 0, 0);
-	fourthText.setButtonColor(255, 0, 0);
-	fifthText.setButtonColor(255, 0, 0);
-
-	firstText.setFont(TTF_OpenFont("Resources/Fonts/Pozo.ttf", 50));
+	firstText.setFont(TTF_OpenFont("arial_narrow_7.ttf", 50));
 	firstText.LoadFromRenderedText("YOU HAVE WON!", renderer,
-				firstText.getButtonColor());
-	secondText.setFont(TTF_OpenFont("Resources/Fonts/Pozo.ttf", 50));
+				firstText.setButtonColor(255, 0, 0));
+	secondText.setFont(TTF_OpenFont("arial_narrow_7.ttf", 50));
 	secondText.LoadFromRenderedText("YOU HAVE WON!", renderer,
-				secondText.getButtonColor());
-	thirdText.setFont(TTF_OpenFont("Resources/Fonts/Pozo.ttf", 50));
+				secondText.setButtonColor(255, 0, 0));
+	thirdText.setFont(TTF_OpenFont("arial_narrow_7.ttf", 50));
 	thirdText.LoadFromRenderedText("YOU HAVE WON!", renderer,
-			thirdText.getButtonColor());
-	fourthText.setFont(TTF_OpenFont("Resources/Fonts/Pozo.ttf", 50));
+			thirdText.setButtonColor(255, 0, 0));
+	fourthText.setFont(TTF_OpenFont("arial_narrow_7.ttf", 50));
 	fourthText.LoadFromRenderedText("YOU HAVE WON!", renderer,
-			fourthText.getButtonColor());
-	fifthText.setFont(TTF_OpenFont("Resources/Fonts/Pozo.ttf", 50));
+			fourthText.setButtonColor(255, 0, 0));
+	fifthText.setFont(TTF_OpenFont("arial_narrow_7.ttf", 50));
 	fifthText.LoadFromRenderedText("YOU HAVE WON!", renderer,
-			fifthText.getButtonColor());
+			fifthText.setButtonColor(255, 0, 0));
 
 	for(int i = 0; i < 300; i += 5){
-			mBackground.loadTextureFromFile("Resources/Images/blueScreen.gif", renderer);
+			mBackground.loadTexture("blueScreen.gif", renderer);
 			SDL_RenderCopy(renderer, mBackground.getKTexture(), NULL, NULL);
 
 			firstText.setPosition(i, i, introButton_width, introButton_height);
@@ -47,7 +54,7 @@ void Win::writeOnScreen(SDL_Renderer* renderer)
 		SDL_RenderPresent(renderer);
 	}
 	for(int i = 0; i < 300; i += 5){
-			mBackground.loadTextureFromFile("Resources/Images/blueScreen.gif", renderer);
+			mBackground.loadTexture("blueScreen.gif", renderer);
 			SDL_RenderCopy(renderer, mBackground.getKTexture(), NULL, NULL);
 
 			firstText.setPosition(300, 300, introButton_width, introButton_height);
@@ -59,7 +66,7 @@ void Win::writeOnScreen(SDL_Renderer* renderer)
 		SDL_RenderPresent(renderer);
 	}
 	for(int i = 0; i < 300; i += 5){
-				mBackground.loadTextureFromFile("Resources/Images/blueScreen.gif", renderer);
+				mBackground.loadTexture("blueScreen.gif", renderer);
 				SDL_RenderCopy(renderer, mBackground.getKTexture(), NULL, NULL);
 
 				firstText.setPosition(300, 300, introButton_width, introButton_height);
@@ -75,7 +82,7 @@ void Win::writeOnScreen(SDL_Renderer* renderer)
 			SDL_RenderPresent(renderer);
 		}
 	for(int i = 0; i < 300; i += 5){
-					mBackground.loadTextureFromFile("Resources/Images/blueScreen.gif", renderer);
+					mBackground.loadTexture("blueScreen.gif", renderer);
 					SDL_RenderCopy(renderer, mBackground.getKTexture(), NULL, NULL);
 
 					firstText.setPosition(300, 300, introButton_width, introButton_height);
@@ -94,25 +101,18 @@ void Win::writeOnScreen(SDL_Renderer* renderer)
 				SDL_RenderPresent(renderer);
 			}
 	for(int i = 0; i < 300; i += 5){
-						mBackground.loadTextureFromFile("Resurces/Images/blueScreen.gif", renderer);
-						SDL_RenderCopy(renderer, getKTexture(), NULL, NULL);
+						mBackground.loadTexture("blueScreen.gif", renderer);
+						SDL_RenderCopy(renderer, mBackground.getKTexture(), NULL, NULL);
 
-						firstText.setPosition(300, 300, introButton_width, introButton_height);
-						firstText.textRender(firstText.getKRect(), firstText.getKTexture(), renderer);
-
-						secondText.setPosition(300, 300, introButton_width, introButton_height);
-						secondText.textRender(secondText.getKRect(), secondText.getKTexture(), renderer);
-
-						thirdText.setPosition(300, 300, introButton_width, introButton_height);
-						thirdText.textRender(thirdText.getKRect(), thirdText.getKTexture(), renderer);
-
-						fourthText.setPosition(300, 300, introButton_width, introButton_height);
-						fourthText.textRender(fourthText.getKRect(), fourthText.getKTexture(), renderer);
-
-						fifthText.setPosition(300, 300, introButton_width + i, introButton_height + i);
+						fifthText.setPosition(300 - i, 300 - i, introButton_width + i*2, introButton_height + i*2);
 						fifthText.textRender(fifthText.getKRect(), fifthText.getKTexture(), renderer);
 
 					SDL_RenderPresent(renderer);
 				}
+
 }
 
+std::string Win::floatToString(float var){
+	std::string dtostr = boost::lexical_cast<std::string> (var);
+	return dtostr.c_str();
+}
