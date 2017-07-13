10a5990fc540216be485aeb1baba46c2b6204c91
Mario Mishev <Mar4omishev@gmail.com>
2017-07-14 00:35:34 +0300

    Fixed some bugs

--- KenoProject/src/Win.h
+++ KenoProject/src/Win.h
@@ -1,24 +1,32 @@
+/*
+ * Win.h
+ *
+ *  Created on: Jul 11, 2017
+ *      Author: Memmory Of Tomorrow
+ */
+
 #ifndef WIN_H_
 #define WIN_H_
 
 #include "BaseObject.h"
 #include "Font.h"
+#include <boost/lexical_cast.hpp>
 
-class Win: public BaseObject
-{
-	public:
-		Win();
-		virtual ~Win();
-	
-		void writeOnScreen(SDL_Renderer*);
-	private:
-		Font firstText;
-      	 	Font secondText;
-      		Font thirdText;
-       		Font fourthText;
-        	Font fifthText;
-		BaseObject mBackground;
-		std::string StringToDouble();
+class Win: public BaseObject{
+public:
+	Win();
+	virtual ~Win();
+
+	void writeOnScreen(SDL_Renderer*);
+private:
+	Font firstText;
+	Font secondText;
+	Font thirdText;
+	Font fourthText;
+	Font fifthText;
+	BaseObject mBackground;
+	std::string floatToString(float var);
+
 };
 
 #endif /* WIN_H_ */
