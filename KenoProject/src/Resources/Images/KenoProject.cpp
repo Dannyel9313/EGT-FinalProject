bdb1a5f0ef2245384872fccaec58ddbd8cae5225
Dannyel9313 <d.sakalev9313@gmail.com>
2017-07-15 14:44:16 +0300

    resources

---
+++ KenoProject/src/Resources/Images/KenoProject.cpp
@@ -0,0 +1,67 @@
+#include "Include.h"
+#include "BaseObject.h"
+#include "GameKeno.h"
+
+int main(int argc, char* args[]) {
+	bool quit = false;
+
+	GameKeno game;
+	SDL_Event e;
+
+	bool introMode = true;
+	bool gameMode = false;
+
+	if (!game.init()) {
+		std::cerr << "Problem with initializing" << std::endl;
+	} else {
+		if (!game.loadMedia()) {
+			std::cerr << "Error loading media" << std::endl;
+		} else {
+			SDL_SetRenderDrawColor(game.getKenoRenderer(), 255, 255, 255, 255);
+			SDL_RenderClear(game.getKenoRenderer());
+//			game.getInfoMode().loadInfoScreen(game.getKenoRenderer());
+			game.getIntroMode().loadIntroScreen(game.getKenoRenderer());
+			while (!quit && introMode == 1) 
+			{
+				while (SDL_PollEvent(&e) != 0) 
+				{
+					if (e.type == SDL_QUIT) 
+					{
+						quit = true;
+					}
+					game.getIntroMode().getVolume().moveVolumeDot(&e);
+					game.getIntroMode().getInsertCredit().setCreditToGame(&e);
+					game.getIntroMode().startNewGameClicked(&gameMode, e);
+				}
+				if (gameMode)
+				{
+					introMode = false;
+				}
+				else if (introMode) 
+				{
+					game.getIntroMode().introScreenPresent(game.getKenoRenderer());
+				}
+				SDL_RenderPresent(game.getKenoRenderer());
+			}
+			game.getGameMode().renderGame(game.getKenoRenderer(), e);
+			while (!quit && gameMode == 1)
+			{
+				while(SDL_PollEvent(&e) != 0)
+				{
+					if(e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+					else if (e.type == SDL_MOUSEBUTTONDOWN)
+                                        {
+                                                game.getGameMode().getNumbersGrid().doIfClicked(game.getKenoRenderer(), e);
+                                                game.getGameMode().renderGame(game.getKenoRenderer(), e);
+                                        }
+				}
+				SDL_RenderPresent(game.getKenoRenderer());
+			}
+		}
+	}
+//	game.close();
+	return 0;
+}
