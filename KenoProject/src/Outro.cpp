d5da91fb0256cff4e24b54310c50613b8937c8d6
Naty <nataly_gr59@abv.bg>
2017-07-16 18:08:20 +0300

    add functionality

--- KenoProject/src/Outro.cpp
+++ KenoProject/src/Outro.cpp
@@ -13,30 +13,58 @@ Outro::~Outro() {
 
 
 void Outro::winingWindow(SDL_Renderer * renderer) {
-	SDL_Rect Message_rect;
-		Message_rect.x = 0;
-		Message_rect.y = 0;
-		Message_rect.w = 800;
-		Message_rect.h = 800;
+	SDL_Rect rect;
+		rect.x = 0;
+		rect.y = 0;
+	    rect.w = 800;
+		rect.h = 700;
 
-		render(renderer, &Message_rect);
+
+
+		//render(renderer, &rect);
+		//SDL_RenderCopy(renderer, background.getKTexture(), NULL, NULL);
+
+
+		background.loadTextureFromFile("back.png",renderer);
+		background.render(renderer,&rect);
+		SDL_RenderPresent(renderer);
 
 }
 
 void Outro::loadFromTTF() {
-	font = TTF_OpenFont("Resources/Fonts/Italic.ttf", 24);
+	font = TTF_OpenFont("Italic.ttf", 24);
 	TTF_SetFontStyle(font, TTF_STYLE_ITALIC);
 }
 
 
 
 void Outro::writingOnScreen(SDL_Renderer * renderer,double money) {
+
+loadFromTTF();
+
 	//std::string str = money;
+	int x=0,y=0;
+
 	SDL_Color color={0,0,0};
-	SDL_Rect rect = {400,500 , 100, 100};
+	SDL_Rect rect = {300,300 , 100, 100};
+	SDL_Rect rect1={300,200,400,400};
+	SDL_Rect rect2={x,y,100,100};
+
+	winPic.loadTextureFromFile("gift.png",renderer);
+	winPic.render(renderer,&rect1);
+
+	for(int i=0;i<100;i++){
+		x+=i;
+		y+=i;
+		loadTextureFromTTF("Congratulation !! You Win ", renderer, font, color);
+
+	}
+	render(renderer, &rect2);
+
 
 	loadTextureFromTTF(doubleToString(money), renderer, font, color);
 	render(renderer, &rect);
+	SDL_RenderPresent(renderer);
 
 }
 
@@ -46,3 +74,17 @@ std::string Outro::doubleToString(double var)
 	return str.c_str();
 }
 
+BaseObject& Outro::getBackground() {
+	return background;
+}
+
+void Outro::setBackground(const BaseObject& back) {
+	background = back;
+}
+
+BaseObject& Outro::getWinPic(){
+	return winPic;
+}
+void Outro::setWinPic(const BaseObject& win){
+	winPic=win;
+}
